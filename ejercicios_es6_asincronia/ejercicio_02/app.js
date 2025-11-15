4a25e7c17facf8b35f2682a725d26e5327ebf794
amna <antoniomiguel2001@hotmail.es>
2025-11-15 04:00:57 +0100

    fix 02

--- ejercicios_es6_asincronia/ejercicio_02/app.js
+++ ejercicios_es6_asincronia/ejercicio_02/app.js
@@ -10,7 +10,6 @@ const pointsList2 = [54, 87, 99, 65, 32];
 const pointsCopy = [...pointsList, ...pointsList2];
 console.log(pointsCopy);
 // -------------------------------
-//https://cdn.discordapp.com/attachments/1238614286901969029/1436429412764618772/66fexhpa6vwd1.png?ex=690f92a3&is=690e4123&hm=1a460e5da706eb330d6e6414fc9955e52b1d30f54d3a6e053c4ef49d272a7d21&
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
 const toysCopy = {...toy, ...toyUpdate};
 console.log(toysCopy);
